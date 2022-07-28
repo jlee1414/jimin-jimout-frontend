1d0f89f13b3b3141b4c9e9ffba7fc5a83f3f64a5
jlee1414 <junniejlee@gmail.com>
2022-07-28 14:33:48 -0700

    updated files with imports

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+
+};
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,3 +1,3 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
-import "./Quiz.css";
+import "./Results.css"
