84a5653e657ac26e87ec41638a1653f10f749d94
jlee1414 <junniejlee@gmail.com>
2022-07-29 18:41:06 -0700

    I don't remember what I did but when I checked to see if anything needed to be pushed, I saw there were unstaged changes so here I am making a new commit and pushing.

--- src/App.js
+++ src/App.js
@@ -6,3 +6,5 @@ import { useEffect, useState } from "react";
 function App() {
 
 };
+
+export default App;
--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,3 +1,6 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 import "./Quiz.css";
+
+// const Quiz = 
+
