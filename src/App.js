90a037611f25da7aa6e79d949263c9cac8155b82
jlee1414 <junniejlee@gmail.com>
2022-08-02 20:08:23 -0700

    tried to figure out how to weigh questions-failed. ALso tried to figure out why nothing is showing up when I do yarn start.

--- src/App.js
+++ src/App.js
@@ -89,44 +89,85 @@ export default function App() {
 		},
 	];
 
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [showScore, setShowScore] = useState(false);
-  const [score, setScore] = useState(0);
+//   const [currentQuestion, setCurrentQuestion] = useState(0);
+//   const [showScore, setShowScore] = useState(false);
+//   const [score, setScore] = useState(0);
 
-  const handleAnswerButtonClick = (isCorrect) => {
-    if (isCorrect) {
-      setScore(score + 1);
-      console.log("We are in a function")
-    }
+//   const handleAnswerButtonClick = (isCorrect) => {
+//     if (isCorrect) {
+//       setScore(score + 1);
+//       console.log("We are in a function")
+//     }
 
-    const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < questions.length) {
-      setCurrentQuestion(nextQuestion);
-    } else {
-      setShowScore(true);
-    }
-  };
+//     const nextQuestion = currentQuestion + 1;
+//     if (nextQuestion < questions.length) {
+//       setCurrentQuestion(nextQuestion);
+//     } else {
+//       setShowScore(true);
+//     }
+//   };
 
+// 	return (
+// 		<div className='app'>
+// 			{showScore ? (
+// 				<div className='score-section'>
+//           You scored {score} out of {questions.length}
+//         </div>
+// 			) : (
+// 				<>
+// 					<div className='question-section'>
+// 						<div className='question-count'>
+// 							<span>Question {currentQuestion+ 1}</span>/{questions.length}
+// 						</div>
+// 						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+// 					</div>
+// 					<div className='answer-section'>
+//             {questions[currentQuestion].answerOptions.map((answerOption, index) =>(
+//               <button onClick={() => handleAnswerButtonClick()}>{answerOption.isCorrect}</button>
+//           )
+//           )
+//           }
+// 					</div>
+// 				</>
+// 			)}
+// 		</div>
+// 	);
+// }
+
+const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
 	return (
 		<div className='app'>
 			{showScore ? (
 				<div className='score-section'>
-          You scored {score} out of {questions.length}
-        </div>
+					You scored {score} out of {questions.length}
+				</div>
 			) : (
 				<>
 					<div className='question-section'>
 						<div className='question-count'>
-							<span>Question {currentQuestion+ 1}</span>/{questions.length}
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
 						</div>
 						<div className='question-text'>{questions[currentQuestion].questionText}</div>
 					</div>
 					<div className='answer-section'>
-            {questions[currentQuestion].answerOptions.map((answerOption, index) =>(
-              <button onClick={() => handleAnswerButtonClick()}>{answerOption.isCorrect}</button>
-          )
-          )
-          }
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
 					</div>
 				</>
 			)}
