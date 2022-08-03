48f50565d60698a30df65879380590b82cea2a2b
jlee1414 <junniejlee@gmail.com>
2022-08-02 21:03:46 -0700

    played around with some quiz code, moved it to App.js. Don't know if this is the correct way.

--- src/App.js
+++ src/App.js
@@ -5,4 +5,24 @@ import axios from "axios";
 
 
 
-export default function App() {}
+export default function App() {
+  // const [currentQuestion, setCurrentQuestion] = useState(0);
+  // const [showScore, setShowScore] = useState(false);
+  // const [score, setScore] = useState(0);
+
+  // const handleAnswerOptionClick = (isCorrect) => {
+  //   if (isCorrect) {
+  //     setScore(score + 1);
+  //   }
+
+  //   // while loop that uses JSON as a frequency table to keep track of which answer the user is feeling
+  //   // and return that feeling to retrieve the corresponding playlist
+
+  //   const nextQuestion = currentQuestion + 1;
+  //   if (nextQuestion < questions.length) {
+  //     setCurrentQuestion(nextQuestion);
+  //   } else {
+  //     setShowScore(true);
+  //   }
+  // };
+  }
--- src/components/quiz.js
+++ src/components/quiz.js
@@ -95,25 +95,25 @@ const Quiz = (props) => {
 	];
 
 // does this go in App.js?
-const [currentQuestion, setCurrentQuestion] = useState(0);
-const [showScore, setShowScore] = useState(false);
-const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
 
-const handleAnswerOptionClick = (isCorrect) => {
-  if (isCorrect) {
-    setScore(score + 1);
-  }
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
 
-  // while loop that uses JSON as a frequency table to keep track of which answer the user is feeling
-  // and return that feeling to retrieve the corresponding playlist
+    // while loop that uses JSON as a frequency table to keep track of which answer the user is feeling
+    // and return that feeling to retrieve the corresponding playlist
 
-  const nextQuestion = currentQuestion + 1;
-  if (nextQuestion < questions.length) {
-    setCurrentQuestion(nextQuestion);
-  } else {
-    setShowScore(true);
-  }
-};
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
 
   return (
     <div className='app'>
