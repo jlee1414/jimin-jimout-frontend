bd1ef1fa2d6985c279918e7cc8d33a68f416ca03
Fena Dang <fenadang@gmail.com>
2022-08-11 14:40:40 -0700

    Fd: Small changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import "./Home.css";
 import Proptypes from "prop-types";
 import { useState } from "react";
-
+import btspic from "src/images/Group_2.png";
+// /Users/fenadang/ada/Capstone/jimin-jimout-frontend/src/images/Group_2.jpeg
 const Home = (props) => {
-  return <h1 id="bts"> BTS </h1>;
+  return (
+    <div>
+      <h1 id="bts"> BTS </h1>
+      <img src="./images/Group_2.png" />
+    </div>
+  );
 };
 
 export default Home;
