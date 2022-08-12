752f6204e830f4bc6d054b493722b2633e4fda16
Fena Dang <fenadang@gmail.com>
2022-08-12 11:14:09 -0700

    FD: Added bio components to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import "./Home.css";
 import Proptypes from "prop-types";
+
 import { useState } from "react";
 import Quiz from "./Quiz";
 import Bio from "./Bio";
@@ -11,15 +12,50 @@ import JiminImage from "./images/Jimin-wow.jpeg";
 import SugaImage from "./images/Suga.jpeg";
 import VImage from "./images/V- GQ.jpeg";
 
+const bios = {
+  Jk: "Jk bio",
+  RM: "RM bio",
+  Jhope: "Jhope bio",
+  Jimin: "Jimin bio",
+  V: "V bio",
+  Suga: "Suga bio",
+  Jin: "Jin bio",
+};
+
 const Home = (props) => {
   const [bioState, setBioState] = useState(false);
+  const [memberBio, setMemberBio] = useState("");
   const switchBio = (event) => {
-    console.log(event.target);
-    // console.log("imageclicked");
     setBioState(true);
+    // console.log(event.target.className);
+    const memeber = event.target.className;
+    // console.log("imageclicked");
+    setMemberBio(bios[memeber]);
+    // console.log(memeberBio);
+  };
+
+  const backToHomePage = () => {
+    setBioState(false);
   };
   if (bioState) {
-    <Bio></Bio>;
+    return (
+      <body className="homeBody">
+        <div className="bts">
+          <h1> BTS </h1>
+        </div>
+        <div>
+          <img
+            className="btsMainPic"
+            src="https://ibighit.com/bts/images/profile/proof/member/bts-pc.jpg"
+            alt="group"
+          />
+        </div>
+        <div>
+          <p>{memberBio}</p>
+        </div>
+        <button onClick={backToHomePage}>Back</button>
+      </body>
+    );
   }
   return (
     <body className="homeBody">
@@ -34,13 +70,23 @@ const Home = (props) => {
         />
       </div>
       <div className="MembersImages">
-        <img className="JkPic" src={JkImage} alt="JK" onClick={switchBio} />
-        <img className="JinPic" src={JinImage} alt="Jin" />
-        <img className="RMPic" src={RMImage} alt="RM" />
-        <img className="JHopePic" src={JHopeImage} alt="JHope" />
-        <img className="JiminPic" src={JiminImage} alt="Jimin" />
-        <img className="SugaPic" src={SugaImage} alt="Suga" />
-        <img className="VPic" src={VImage} alt="V" />
+        <img className="Jk" src={JkImage} alt="JK" onClick={switchBio} />
+        <img className="Jin" src={JinImage} alt="Jin" onClick={switchBio} />
+        <img className="RM" src={RMImage} alt="RM" onClick={switchBio} />
+        <img
+          className="JHope"
+          src={JHopeImage}
+          alt="JHope"
+          onClick={switchBio}
+        />
+        <img
+          className="Jimin"
+          src={JiminImage}
+          alt="Jimin"
+          onClick={switchBio}
+        />
+        <img className="Suga" src={SugaImage} alt="Suga" onClick={switchBio} />
+        <img className="V" src={VImage} alt="V" onClick={switchBio} />
       </div>
     </body>
   );
